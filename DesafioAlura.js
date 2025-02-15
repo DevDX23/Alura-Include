2c5eb0a9dadb3e59c40583446c9c2df941b9f6f4
DevDX23 <dev.dxbdan@gmail.com>
2025-02-15 16:08:11 -0300

    Atividade

---
+++ DesafioAlura.js
@@ -0,0 +1,11 @@
+alert("Boas vindas ao nosso site!")
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50
+let saldoDisponivel = 1000
+alert("Erro! Preencha todos os dados")
+let mensagemDeErro = "Erro! Preencha todos os dados"
+alert(mensagemDeErro)
+nome = prompt("Digite seu nome: ")
+idade = prompt("Digite sua idade: ")
+if
